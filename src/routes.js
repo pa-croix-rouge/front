0dd9ec388f4813adda1abe20207d47c29297a401
Quentin MOLERO <quentin.molero@outlook.com>
2023-06-27 14:37:28 +0200

    Add page for beneficiaries

--- src/routes.js
+++ src/routes.js
@@ -11,64 +11,57 @@ import {CalendarIcon, TimeIcon} from "@chakra-ui/icons";
 import ULDashboard from "./views/Dashboard/ULDashboard";
 import ManageEvents from "./views/Dashboard/ManageEvents";
 import Stocks from "./views/Dashboard/Stocks";
-import FoodStocks from "./views/Dashboard/FoodStocks";
-import ClothStocks from "./views/Dashboard/ClothStocks";
 import Roles from "./views/Dashboard/roles/Roles";
+import Beneficiaries from "./views/Dashboard/Beneficiaries";
+import {FaChartBar, FaShoppingCart, FaUsers, FaWrench} from "react-icons/fa";
 
 var dashRoutes = [
   {
     path: "/ul-dashboard",
-    name: "UL Dashboard",
-    icon: <HomeIcon color='inherit' />,
+    name: "Tableau de bord",
+    icon: <FaChartBar color='inherit' />,
     component: ULDashboard,
     layout: "/admin",
   },
   {
+    path: "/local-unit",
+    name: "Unité locale",
+    icon: <HomeIcon color='inherit' />,
+    component: LocalUnit,
+    layout: "/admin",
+  },
+  {
+    path: "/beneficiaries",
+    name: "Bénéficiaires",
+    icon: <FaUsers color='inherit' />,
+    component: Beneficiaries,
+    layout: "/admin",
+  },
+  {
     path: "/events",
-    name: "Events",
+    name: "Événements",
     icon: <TimeIcon color='inherit' />,
     component: Events,
     layout: "/admin",
   },
   {
     path: "/manage-events",
-    name: "Manage Events",
+    name: "Gestion événements",
     icon: <CalendarIcon color='inherit' />,
     component: ManageEvents,
     layout: "/admin",
   },
   {
-    path: "/local-unit",
-    name: "Local Unit",
-    icon: <HomeIcon color='inherit' />,
-    component: LocalUnit,
-    layout: "/admin",
-  },
-  {
     path: "/stocks",
     name: "Stocks",
-    icon: <HomeIcon color='inherit' />,
+    icon: <FaShoppingCart color='inherit' />,
     component: Stocks,
-    layout: "/admin",
-  },
-  {
-    path: "/food-stocks",
-    name: "Food Stocks",
-    icon: <HomeIcon color='inherit' />,
-    component: FoodStocks,
-    layout: "/admin",
-  },
-  {
-    path: "/cloth-stocks",
-    name: "Cloth Stocks",
-    icon: <HomeIcon color='inherit' />,
-    component: ClothStocks,
     layout: "/admin",
   },
   {
     path: "/roles",
     name: "Roles",
-    icon: <HomeIcon color='inherit' />,
+    icon: <FaWrench color='inherit' />,
     component: Roles,
     layout: "/admin",
   },
--- src/views/Dashboard/ClothStocks.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ClothStocks() {
-    return (
-        <div>
-            <h1>Cloth Stocks</h1>
-        </div>
-    )
-}
